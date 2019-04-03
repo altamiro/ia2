aed90e4e3ca0e0fde8c6c66cbb7b4be36f31b967
tarcysyo <45829765+tarcysyo@users.noreply.github.com>
2019-04-02 22:04:02 -0300

    Adama x Adadelta
    Criação de um arquivo .py e deleção de outros diretórios

--- Flower_Recognition.py
+++ Flower_Recognition.py
@@ -1,57 +1,41 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-Este é um arquivo de script temporário.
-"""
-import warnings
-warnings.filterwarnings('always')
-warnings.filterwarnings('ignore')
-
+#Imports
 # data visualisation and manipulation
-#%matplotlib inline 
 import numpy as np
-#import pandas as pd
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from matplotlib import style
 import seaborn as sns
-
+ 
+#configure
+# sets matplotlib to inline and displays graphs below the corressponding cell.  
 style.use('fivethirtyeight')
 sns.set(style='whitegrid',color_codes=True)
 
 #model selection
 from sklearn.model_selection import train_test_split
-#from sklearn.model_selection import KFold
-#from sklearn.metrics import accuracy_score,precision_score,recall_score,confusion_matrix,roc_curve,roc_auc_score
-#from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import LabelEncoder
 
 #preprocess.
-#from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import ImageDataGenerator
 
 #dl libraraies
-#from keras import backend as K
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.optimizers import Adam,SGD,Adagrad,Adadelta,RMSprop
+from keras.optimizers import Adamax,Adadelta
 from keras.utils import to_categorical
 
 # specifically for cnn
-from keras.layers import Dropout, Flatten,Activation
+from keras.layers import Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
  
 import tensorflow as tf
 import random as rn
 
 # specifically for manipulating zipped images and getting numpy arrays of pixel values of images.
-import cv2                  
-#import numpy as np  
+import cv2                    
 from tqdm import tqdm
 import os                   
-#from random import shuffle  
-#from zipfile import ZipFile
-#from PIL import Image
 
+#
 X=[]
 Z=[]
 IMG_SIZE=150
@@ -63,7 +47,7 @@ FLOWER_ROSE_DIR='flowers/rose'
 
 def assign_label(img,flower_type):
     return flower_type
-    
+
 def make_train_data(flower_type,DIR):
     for img in tqdm(os.listdir(DIR)):
         label=assign_label(img,flower_type)
@@ -75,19 +59,20 @@ def make_train_data(flower_type,DIR):
         Z.append(str(label))
 
 make_train_data('Daisy',FLOWER_DAISY_DIR)
-print(len(X))
-
 make_train_data('Sunflower',FLOWER_SUNFLOWER_DIR)
-print(len(X))
-
 make_train_data('Tulip',FLOWER_TULIP_DIR)
-print(len(X))
-
 make_train_data('Dandelion',FLOWER_DANDI_DIR)
-print(len(X))
-
 make_train_data('Rose',FLOWER_ROSE_DIR)
-print(len(X))
+
+#fig,ax=plt.subplots(3,2)
+#fig.set_size_inches(15,15)
+#for i in range(3):
+#    for j in range (2):
+#        l=rn.randint(0,len(Z))
+#        ax[i,j].imshow(X[l])
+#        ax[i,j].set_title('Flower: '+Z[l])
+#        
+#plt.tight_layout()
 
 le=LabelEncoder()
 Y=le.fit_transform(Z)
@@ -101,6 +86,166 @@ np.random.seed(42)
 rn.seed(42)
 tf.set_random_seed(42)
 
+datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range = 0.1, # Randomly zoom image 
+        width_shift_range=0.2,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.2,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+
+datagen.fit(x_train)
+
+batch_size=128
+epochs=50
+
+###############################################################################
+#ADAMAX sem dropout
+# # modelling starts using a CNN.
+model = Sequential()
+model.add(Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same',activation ='relu', input_shape = (150,150,3)))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(filters = 64, kernel_size = (3,3),padding = 'Same',activation ='relu'))
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+ 
+model.add(Conv2D(filters =96, kernel_size = (3,3),padding = 'Same',activation ='relu'))
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+
+model.add(Conv2D(filters = 96, kernel_size = (3,3),padding = 'Same',activation ='relu'))
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(256, activation = 'relu'))
+model.add(BatchNormalization())
+model.add(Dense(5, activation = 'softmax'))
+
+model.compile(optimizer=Adamax(lr=0.002),loss='categorical_crossentropy',metrics=['accuracy'])
+
+model.summary()
+
+History = model.fit_generator(datagen.flow(x_train,y_train, batch_size=batch_size),
+                              epochs = epochs, validation_data = (x_test,y_test),
+                              steps_per_epoch=x_train.shape[0] // batch_size)
+
+plt.plot(History.history['loss'])
+plt.plot(History.history['val_loss'])
+plt.title('Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epochs')
+plt.legend(['train', 'test'])
+plt.show()
+
+plt.plot(History.history['acc'])
+plt.plot(History.history['val_acc'])
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epochs')
+plt.legend(['train', 'test'])
+plt.show()
+
+###############################################################################
+
+#ADAMAX com dropout
+# # modelling starts using a CNN.
+
+model = Sequential()
+model.add(Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same',activation ='relu', input_shape = (150,150,3)))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(filters = 64, kernel_size = (3,3),padding = 'Same',activation ='relu'))
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+ 
+model.add(Conv2D(filters =96, kernel_size = (3,3),padding = 'Same',activation ='relu'))
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+
+model.add(Conv2D(filters = 96, kernel_size = (3,3),padding = 'Same',activation ='relu'))
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+
+model.add(Flatten())
+model.add(Dropout(0.5))
+model.add(Dense(256, activation = 'relu'))
+model.add(BatchNormalization())
+model.add(Dense(5, activation = 'softmax'))
+
+model.compile(optimizer=Adamax(lr=0.002),loss='categorical_crossentropy',metrics=['accuracy'])
+
+model.summary()
+
+History = model.fit_generator(datagen.flow(x_train,y_train, batch_size=batch_size),
+                              epochs = epochs, validation_data = (x_test,y_test),
+                              steps_per_epoch=x_train.shape[0] // batch_size)
+# model.fit(x_train,y_train,epochs=epochs,batch_size=batch_size,validation_data = (x_test,y_test))
+
+plt.plot(History.history['loss'])
+plt.plot(History.history['val_loss'])
+plt.title('Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epochs')
+plt.legend(['train', 'test'])
+plt.show()
+
+plt.plot(History.history['acc'])
+plt.plot(History.history['val_acc'])
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epochs')
+plt.legend(['train', 'test'])
+plt.show()
+
+################################################################################
+#ADADELTA sem dropout
+# # modelling starts using a CNN.
+
+model = Sequential()
+model.add(Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same',activation ='relu', input_shape = (150,150,3)))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(filters = 64, kernel_size = (3,3),padding = 'Same',activation ='relu'))
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+ 
+model.add(Conv2D(filters =96, kernel_size = (3,3),padding = 'Same',activation ='relu'))
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+
+model.add(Conv2D(filters = 96, kernel_size = (3,3),padding = 'Same',activation ='relu'))
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(256, activation = 'relu'))
+model.add(BatchNormalization())
+model.add(Dense(5, activation = 'softmax'))
+
+model.compile(optimizer=Adadelta(lr=1.0),loss='categorical_crossentropy',metrics=['accuracy'])
+
+model.summary()
+
+History = model.fit_generator(datagen.flow(x_train,y_train, batch_size=batch_size),
+                              epochs = epochs, validation_data = (x_test,y_test), 
+                              steps_per_epoch=x_train.shape[0] // batch_size)
+
+plt.plot(History.history['loss'])
+plt.plot(History.history['val_loss'])
+plt.title('Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epochs')
+plt.legend(['train', 'test'])
+plt.show()
+
+plt.plot(History.history['acc'])
+plt.plot(History.history['val_acc'])
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epochs')
+plt.legend(['train', 'test'])
+plt.show()
+
+###############################################################################
+#ADADELTA com dropout
 # # modelling starts using a CNN.
 
 model = Sequential()
@@ -119,34 +264,31 @@ model.add(Conv2D(filters = 96, kernel_size = (3,3),padding = 'Same',activation =
 model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
 
 model.add(Flatten())
-model.add(Dense(512))
-model.add(Activation('relu'))
-model.add(Dense(5, activation = "softmax"))
+model.add(Dropout(0.5))
+model.add(Dense(256, activation = 'relu'))
+model.add(BatchNormalization())
+model.add(Dense(5, activation = 'softmax'))
 
-batch_size=128
-epochs=25
-
-from keras.callbacks import ReduceLROnPlateau
-red_lr= ReduceLROnPlateau(monitor='val_acc',patience=5,verbose=0, factor=0.1)
-
-#datagen = ImageDataGenerator(
-#        featurewise_center=False,  # set input mean to 0 over the dataset
-#        samplewise_center=False,  # set each sample mean to 0
-#        featurewise_std_normalization=False,  # divide inputs by std of the dataset
-#        samplewise_std_normalization=False,  # divide each input by its std
-#        zca_whitening=False,  # apply ZCA whitening
-#        rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
-#        zoom_range = 0.1, # Randomly zoom image 
-#        width_shift_range=0.2,  # randomly shift images horizontally (fraction of total width)
-#        height_shift_range=0.2,  # randomly shift images vertically (fraction of total height)
-#        horizontal_flip=True,  # randomly flip images
-#        vertical_flip=False)  # randomly flip images
-#
-#
-#datagen.fit(x_train)
-
-model.compile(optimizer=Adam(lr=0.001),loss='categorical_crossentropy',metrics=['accuracy'])
+model.compile(optimizer=Adadelta(lr=1.0),loss='categorical_crossentropy',metrics=['accuracy'])
 
 model.summary()
 
+History = model.fit_generator(datagen.flow(x_train,y_train, batch_size=batch_size),
+                              epochs = epochs, validation_data = (x_test,y_test),
+                              steps_per_epoch=x_train.shape[0] // batch_size)
 
+plt.plot(History.history['loss'])
+plt.plot(History.history['val_loss'])
+plt.title('Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epochs')
+plt.legend(['train', 'test'])
+plt.show()
+
+plt.plot(History.history['acc'])
+plt.plot(History.history['val_acc'])
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epochs')
+plt.legend(['train', 'test'])
+plt.show()
